c055280d3d58d6e4bf8918c6e77a8f75c0cdba1f
Jiří Kulík <jiri.kulik@jkulik.eu>
2018-11-24 15:30:19 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
+
+#No machine - adding a user for login to desktop
 groupadd -r $USER -g 433
 useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER
 adduser $USER sudo
 mkdir -p /home/$USER
-cp -a /activateconda.sh /home/$USER
-#chown -R $USER:$USER /home/$USER moved below
+#chown -R $USER:$USER /home/$USER #moved below
 echo $USER':'$PASSWORD | chpasswd
+
+#Script to activate conda (without that works for
+#root only)
+cp -a /activateconda.sh /home/$USER
 chmod +x /home/$USER/activateconda.sh
 
+#SSH keys
 mkdir -p /home/$USER/.ssh
 chmod 0700 /home/$USER/.ssh
 cp -a /root/.ssh/jiri_kulik /home/$USER/.ssh/jiri_kulik
@@ -15,11 +21,13 @@ cp -a /root/.ssh/jiri_kulik.pub /home/$USER/.ssh/jiri_kulik.pub
 chmod 600 /home/$USER/.ssh/jiri_kulik
 chmod 600 /home/$USER/.ssh/jiri_kulik.pub
 
+#Change owner of the user's home folder for the user
 chown -R $USER:$USER /home/$USER
 
 #su $USER
 #eval $(ssh-agent -s)
-su -c "eval $(ssh-agent -s) && ssh-add /home/$USER/.ssh/jiri_kulik" $USER
+su -c "eval $(ssh-agent -s)" $USER
+su -c "ssh-add /home/$USER/.ssh/jiri_kulik" $USER
 
 #su root
 /etc/NX/nxserver --startup
